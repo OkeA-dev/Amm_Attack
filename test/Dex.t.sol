19c1de0070e77351f79e973df62a308ad625b0ff
D4n13l-D33 <abrahamdanielu793@gmail.com>
2025-08-18 18:33:26 +0100

    push

---
+++ test/Dex.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Dex, SwappableToken} from "../src/Dex.sol";
+
+contract DexTest is Test {
+    SwappableToken public swappabletoken1;
+    SwappableToken public swappabletoken2;
+    Dex public dex;
+    address attacker = makeAddr("attacker");
+
+    function setUp() public {
+        dex = new Dex();
+        swappabletoken1 = new SwappableToken(address(dex),"Swap","SW", 110);
+        vm.label(address(swappabletoken1), "Token 1");
+        swappabletoken2 = new SwappableToken(address(dex),"Swap","SW", 110);
+        vm.label(address(swappabletoken2), "Token 2");
+        dex.setTokens(address(swappabletoken1), address(swappabletoken2));
+
+        dex.approve(address(dex), 100);
+        dex.addLiquidity(address(swappabletoken1), 100);
+        dex.addLiquidity(address(swappabletoken2), 100);
+
+        vm.label(attacker, "Attacker");
+        
+        // Set up the attacker with some initial balance
+        swappabletoken1.transfer(attacker, 10);
+        swappabletoken2.transfer(attacker, 10);
+        
+
+        //DO_NOT_TOUCH
+    }
+
+    function test_Exploit() public {
+       //Execute the attacker here.
+
+        is_Drained();
+    }
+
+    function is_Drained () internal view{
+         require(swappabletoken1.balanceOf(address(dex)) == 0);
+    }
+
+}
---
+++ test/DexTwo.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DexTwo, SwappableTokenTwo} from "../src/DexTwo.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DexTwoTest is Test {
+    SwappableTokenTwo public swappabletoken1;
+    SwappableTokenTwo public swappabletoken2;
+    
+    DexTwo public dexTwo;
+    address attacker = makeAddr("attacker");
+
+    function setUp() public {
+        dexTwo = new DexTwo();
+        swappabletoken1 = new SwappableTokenTwo(address(dexTwo),"Swap","SW", 110);
+        vm.label(address(swappabletoken1), "Token 1");
+        swappabletoken2 = new SwappableTokenTwo(address(dexTwo),"Swap","SW", 110);
+        vm.label(address(swappabletoken2), "Token 2");
+        dexTwo.setTokens(address(swappabletoken1), address(swappabletoken2));
+
+        dexTwo.approve(address(dexTwo), 100);
+        dexTwo.add_liquidity(address(swappabletoken1), 100);
+        dexTwo.add_liquidity(address(swappabletoken2), 100);
+
+        vm.label(attacker, "Attacker");
+        // Set up the attacker with some initial balance
+        swappabletoken1.transfer(attacker, 10);
+        swappabletoken2.transfer(attacker, 10);
+
+        //DO_NOT_TOUCH
+    }
+
+    function test_Exploit() public {
+       //Execute the attacker here.
+
+        is_Drained();
+    }
+
+    function is_Drained () internal view{
+        require(swappabletoken1.balanceOf(address(dexTwo)) == 0);
+        require(swappabletoken2.balanceOf(address(dexTwo)) == 0);
+    }
+
+}
+
